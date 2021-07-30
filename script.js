46587e1ba4f1187e35de491a4147b279744b4d83
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-30 10:10:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const messageInput = document.getElementById("message-input");
+document.querySelector("#message-input").addEventListener("keydown", (event) => {
+  if(event.key === "Enter")
+    getMessage();
+});
 
-messageInput.addEventListener("keydown", function(event){
-  if (event.key == "Enter")
+document.querySelector("#submit").addEventListener("click", () => {
     getMessage();
 });
 
 getMessage = () => {
-  document.getElementById("message-output").innerHTML = messageInput.value;
-  messageInput.value = "";
+  document.querySelector("#message-output").textContent = document.querySelector("#message-input").value;
+  document.querySelector("#message-input").value = "";
 }
